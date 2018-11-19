05f0e158373917650b086dee9a9d4603903fd378
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-11-19 23:08:23 +0300

    NamespaceInfo added

---
+++ AssemblyBrowser/NamespaceInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser
+{
+    public class NamespaceInfo
+    {
+        public NamespaceInfo(string name)
+        {
+            Name = name;
+            Classes = new List<ClassInfo>();
+        }
+
+        public string Name { get; set; }
+        public List<ClassInfo> Classes { get; set; }
+    }
+}
