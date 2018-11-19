b64ab992b132c6630b329f9dc36d12370c4a367d
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-11-19 23:23:18 +0300

    AssemblyInfo added

---
+++ AssemblyBrowser/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser
+{
+    public class AssemblyInfo
+    {
+        public AssemblyInfo()
+        {
+            Namespaces = new List<NamespaceInfo>();
+        }
+
+        public List<NamespaceInfo> Namespaces { get; set; }
+    }
+}
