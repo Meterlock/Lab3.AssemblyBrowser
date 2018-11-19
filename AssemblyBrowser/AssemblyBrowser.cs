cd8a6ee1aef031068d4069121c637c5deef61e69
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-11-19 23:52:42 +0300

    AssemblyBrowser class added

---
+++ AssemblyBrowser/AssemblyBrowser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyBrowser
+{
+    public class AssemblyBrowser
+    {
+        public AssemblyInfo Browse(string filename)
+        {
+            Assembly assembly;
+            AssemblyInfo result = new AssemblyInfo();
+            Type[] types;
+            NamespaceInfo searchableNamesapce;
+
+            assembly = Assembly.LoadFrom(filename);
+            types = assembly.GetTypes();
+            foreach (var type in types)
+            {
+                searchableNamesapce = result.Namespaces.Find(i => i.Name == type.Namespace);
+                if (searchableNamesapce == null)
+                {
+                    searchableNamesapce = new NamespaceInfo(type.Namespace);
+                    result.Namespaces.Add(searchableNamesapce);
+                }
+                searchableNamesapce.Classes.Add(new ClassInfo(type));
+            }
+
+            return result;
+        }
+    }
+}
