0616385404b57a045f645feda7947560c4e14974
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-11-20 12:37:03 +0300

    ViewModel layer implemented
    ICommand and INotifyPropertyChanged realized

---
+++ AssemblyBrowserWPF/AssemblyBrowserViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AssemblyBrowserWPF
+{
+    public class AssemblyBrowserViewModel : INotifyPropertyChanged
+    {
+        private string filename;
+        private AssemblyBrowser.AssemblyInfo result;
+        public event PropertyChangedEventHandler PropertyChanged;
+        private DelegateCommand openFileCommand;
+
+        public string Filename
+        {
+            get
+            {
+                return Path.GetFileName(filename);
+            }
+            set
+            {
+                filename = value;
+                OnPropertyChanged(nameof(Filename));
+            }
+        }
+
+        public AssemblyBrowser.AssemblyInfo Result
+        {
+            get
+            {
+                return result;
+            }
+            set
+            {
+                result = value;
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+
+        public DelegateCommand OpenFileCommand
+        {
+            get
+            {
+                return openFileCommand ?? (openFileCommand = new DelegateCommand(OpenFile));
+            }
+        }
+
+        public void OnPropertyChanged(string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        public void OpenFile(object obj)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Library Files (*.dll)|*.dll";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Filename = openFileDialog.FileName;
+                    Result = new AssemblyBrowser.AssemblyBrowser().Browse(Filename);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+    }
+}
